98be417f567a044db664dc2290b1fa66357b5f37
andy <andy@ks3291861.kimsufi.com>
2013-04-26 17:12:03 +0200

    add example plugin

---
+++ plugins/example.rb
@@ -0,0 +1,10 @@
+class Example < RServ::Plugin
+  def initialize
+    $event.add(self, :on_start, "server::connected")
+  end
+  
+  def on_start
+    # code here will be executed upon link establishment
+    # use send() to send raw text to the protocol
+  end
+end
