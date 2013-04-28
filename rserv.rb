2dc0c3584c8b02a53e56f7f6c5d362e0c33604f0
Andrew Northall <andy@somasonic.org>
2013-04-28 02:42:34 +0100

    default log level: info

--- rserv.rb
+++ rserv.rb
@@ -10,8 +10,7 @@ require 'lib/plugins'
 
 # Basic initialization: config, log, events
 $log = Logger.new('logs/rserv.log')
-#$log = Logger.new(STDOUT)
-$log.level = Logger::DEBUG
+$log.level = Logger::INFO
 $log.info "---------------------"
 $log.info "RServ session started"
 $log.info "---------------------"
