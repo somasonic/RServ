682c6f924bc7403d56a7246632d593f4d03f5fe1
Andrew Northall <andy@somasonic.org>
2013-04-28 02:42:05 +0100

    add user tracking properly YAY :D, still stuff todo

--- lib/irc/user.rb
+++ lib/irc/user.rb
@@ -2,8 +2,8 @@ module RServ::IRC
   
   class User
     
-    attr_reader :sid, :uid, :hops
-    attr_accessor :nick, :mode, :username, :hostname, :ip, :gecos, :account, :realhost, :away
+    attr_reader :sid, :uid, :hops, :away, :away_reason
+    attr_accessor :nick, :mode, :username, :hostname, :ip, :gecos, :account, :realhost
   
     def initialize(nick, uid, hops, mode, username, hostname, ip, gecos)
       @nick, @uid, @hops, @mode = nick, uid, hops, mode
@@ -14,6 +14,19 @@ module RServ::IRC
       @away = false 
       
       @sid = uid[0..2]
+    end
+    
+    def to_s
+      @nick
+    end
+    
+    def hostmask
+      "#{@nick}!#{@username}@#{@hostname}"
+    end
+    
+    def away=(away = true)
+      raise TypeError, 'Away status must be boolean' unless away == true or away == false
+      @away = away
     end
     
   end
--- lib/protocols/ts6.rb
+++ lib/protocols/ts6.rb
@@ -11,7 +11,7 @@ module RServ::Protocols
 			@link = nil #socket
       @established = false
       @remote_sid = nil
-      @to_pong = Array.new #array used to collect servers to pong on burst. currently a hack.
+      @remote = nil
       
       @servers = Hash.new
       @users = Hash.new
@@ -49,7 +49,11 @@ module RServ::Protocols
 			$log.debug("<---| #{line}")
       if @established
         
-        if line =~ /^PING :.*$/
+        if line =~ /^:[0-9A-Z]{9}/
+          user_input(line)
+        elsif line =~ /^:[0-9A-Z]{3}/
+          server_input(line)
+        elsif line =~ /^PING :.*$/
           send(":#{sid} PONG #{name} :#{@remote_sid}")
           $log.info "Ponging #{$1}"
         elsif line =~ /^SQUIT (\w{3}) :(.*)$/
@@ -59,12 +63,14 @@ module RServ::Protocols
       else
         
         #establishing the link
-        if line =~ /^PASS (\S+) TS 6 :(\w{3})$/ # todo: make match accept password to config
+        if line =~ /^:[0-9A-Z]{9}/
+          user_input(line)
+        elsif line =~ /^PASS (\S+) TS 6 :(\w{3})$/ # todo: make match accept password to config
           @remote_sid = $2
           if Configru.link.recvpassword == $1
             $log.info "Password received and matched."
           else
-            $log.fatal "Received conflicting link password, #{$1} received from upstream SID #{$2}. Exiting."
+            $log.fatal "Received incorrect link password from upstream SID #{$2}. Exiting."
             exit
           end
         elsif line =~ /^PING :(\S+)$/  
@@ -80,16 +86,16 @@ module RServ::Protocols
             send(":#{sid} SJOIN #{Time.now.to_i} ##{chan} +nt :#{sid}SRV000")
           end
           send("PING :#{sid}") # ping upstream
-          @to_pong.each do # ping other servers
-            |srv|
-            send(":#{sid} PONG #{name} :#{srv}")
+
+          @servers.each do  #pong each server
+            |remotesid, obj|
+            send(":#{sid} PONG #{name} :#{remotesid}")
           end
+          
           Configru.channels.each do # op ourselves
             |chan|
             send(":#{sid} TMODE 1 ##{chan} +o RServ")
           end
-        elsif line =~ /^:(\w{3}) PING (\S+\.\w+) :(\w{3})$/
-          @to_pong << $1 #hack
         elsif line =~ /^:(\w{3}) PONG (\S+\.\w+) :(\w{3})$/
           if $1 == @remote_sid and $3 == sid # from our upstream only
             @established = true
@@ -99,11 +105,12 @@ module RServ::Protocols
         elsif line =~ /^:(\w{3}) UID (\S+) (\d{1,2}) (\d{10}) \+([a-zA-Z]*) (\S+) (\S+) (\S+) ([0-9]\w{2}[A-Z][A-Z0-9]{5}) :(.*)$/
           #uid
           user = RServ::IRC::User.new($2, $9, $3, $5, $6, $7, $8, $10)
-          puts "New user #{user.uid} on #{user.sid}. Host: #{user.nick}!#{user.username}@#{user.hostname} (#{user.ip}) | Modes: +#{user.mode}"
+          $log.info "New user #{user.uid} on #{user.sid}. Host: #{user.nick}!#{user.username}@#{user.hostname} (#{user.ip}) | Modes: +#{user.mode}"
           @users[user.uid] = user
         elsif line =~ /^:(\w{3}) SID (\S+) (\d{1,2}) ([0-9][0-9A-Z]{2}) :(.*)$/
           server = RServ::IRC::Server.new($4, $2, $3, $5)
-          puts "New server: #{server.hostname} (#{server.sid}) [#{server.gecos}]"
+          $log.info "New server: #{server.hostname} (#{server.sid}) [#{server.gecos}]"
+          @remote = server if server.sid == @remote_sid
           @servers[server.sid] = server
         end
       end
@@ -115,6 +122,48 @@ module RServ::Protocols
 			$log.debug("--->| #{text}")
 			@link.send(text) if @link
 		end
+    
+    def user_input(line)
+      sid = Configru.link.serverid
+      name = Configru.link.name
+      
+      if line =~ /^:(\w{9}) AWAY$/
+        $log.debug "#{$1} AWAY: #{@users[$1].away}"
+        $log.info "User #{$1} (#{@users[$1]}) is no longer away."
+        @users[$1].away = false
+      elsif line =~ /^:(\w{9}) AWAY :(.*)$/
+        $log.info "User #{$1} (#{@users[$1]}) is away (#{$2})."
+        @users[$1].away = true      
+      elsif line =~ /^:(\w{9}) NICK (\S+) :(\d+)$/
+        uid = $1
+        oldnick = @users[uid].nick
+        @users[uid].nick = $2
+        $log.info "Nick change for #{$1}: #{oldnick} -> #{@users[uid].nick}"
+      elsif line =~ /^:(\w{9}) QUIT :(.*)$/
+        $log.info "User #{@users[$1].nick} quit (#{$2})"
+        @users.delete $1
+      elsif
+        $log.info "Unhandled user input: #{line}"
+      end
+    end
+  
+    def server_input(line)
+      sid = Configru.link.serverid
+      name = Configru.link.name
+      
+      if line =~ /^:(\w{3}) UID (\S+) (\d{1,2}) (\d{10}) \+([a-zA-Z]*) (\S+) (\S+) (\S+) ([0-9]\w{2}[A-Z][A-Z0-9]{5}) :(.*)$/
+        user = RServ::IRC::User.new($2, $9, $3, $5, $6, $7, $8, $10)
+        $log.info "New user #{user.uid} on #{user.sid}. Host: #{user.nick}!#{user.username}@#{user.hostname} (#{user.ip}) | Modes: +#{user.mode}"
+        @users[user.uid] = user
+      elsif line =~ /^:(\w{3}) SID (\S+) (\d{1,2}) ([0-9][0-9A-Z]{2}) :(.*)$/
+        server = RServ::IRC::Server.new($4, $2, $3, $5)
+        $log.info "New server: #{server.hostname} (#{server.sid}) [#{server.gecos}]"
+        @servers[server.sid] = server
+        send(":#{sid} PING #{name} :#{server.sid}")
+      elsif
+        $log.info "Unhandled server input: #{line}"
+      end
+    end
     
 	end
 end
