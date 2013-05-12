2c07202a2dcbf46dc11af09c6532d8c6b03c71f4
Andrew Northall <andy@somasonic.org>
2013-05-12 16:28:37 +0100

    make code more readable

--- lib/protocols/ts6.rb
+++ lib/protocols/ts6.rb
@@ -149,69 +149,78 @@ module RServ::Protocols
         $event.send("user::away", @users[$1])
         $log.info "User #{$1} (#{@users[$1]}) is no longer away."
         @users[$1].away = false
+        
       elsif line =~ /^:(\w{9}) AWAY :(.*)$/
         $log.info "User #{$1} (#{@users[$1].nick}) is away (#{$2})."
         @users[$1].away = true      
-        $event.send("user::back", @users[$1])
+        $event.send("user::unaway", @users[$1])
+        
       elsif line =~ /^:(\w{9}) NICK (\S+) :(\d+)$/
-        uid = $1
-        oldnick = @users[uid].nick
-        @users[uid].nick = $2
-        $event.send("user::nick", @users[uid])
-        $log.info "Nick change for #{$1}: #{oldnick} -> #{@users[uid].nick}"
+        $log.info "Nick change for #{$1}: #{@users[$1]} -> #{$2}"
+        
+        old_nick = @users[$1]
+        @users[$1].nick = $2
+        $event.send("user::nick", @users[uid], old_nick)
+      
       elsif line =~ /^:(\w{9}) QUIT :(.*)$/
-        $log.info "User #{@users[$1].nick} quit (#{$2})"
-        $event.send("user::quit", @users[$1])
+        $log.info "User #{@users[$1].nick} quit (#{$2})."
+        $event.send("user::quit", @users[$1], $2)
         @users.delete $1
-        chans = @channels
-        chans.each do
+        
+        # remove user from any channels they were in
+        @channels.each do
           |chan|
           chan.part($1)
         end
-        @channels = chans
+        
       elsif line =~ /^:(\w{9}) ENCAP \S{1} REALHOST (.*)$/
         @users[$1].realhost = $2
-        $log.info "Realhost for #{$1} (#{@users[$1]}) is #{@users[$1].realhost}."
+        $log.debug "Realhost for #{$1} (#{@users[$1]}) is #{@users[$1].realhost}."
+        
       elsif line =~ /^:(\w{9}) ENCAP \S{1} LOGIN (.*)$/
         @users[$1].account = $2
         $event.send("user::login", @users[$1])
         $log.info "#{@users[$1]} logged in as #{@users[$1].account}."
+        
       elsif line =~ /^:(\w{9}) ENCAP \S{1} CERTFP (.*)$/
-        #do nothing
+        #do nothing, prevent unhandled message
+        
       elsif line =~ /^:(\w{9}) JOIN (\d+) (#\w+) (\+.*)$/
-        chan = @channels[$3]
-        chan.join($1)
+        chan = @channels[$3].join($1)
+        
         if $2.to_i < chan.ts
           chan.ts = $2
           chan.mode = $4
           $log.info "New TS for #{chan}: #{chan.ts}. New modes: #{chan.mode}."
         end
-        nick = @users[$1].nick
-        $log.info("#{nick} joined #{chan}.")
+
+        $log.info("#{@users[$1]} joined #{chan}.")
         @channels[$3] = chan 
+        
       elsif line =~ /^:(\w{9}) KICK (#\w+) (\w{9}) :(.*)$/
-        chan = @channels[$2]
-        chan.part($3)
-        nick = @users[$3].nick
-        $log.info("#{nick} kicked from #{chan} by #{@users[$1].nick} (#{$4})")
+        chan = @channels[$2].part($3)
+        $log.info("#{@users[$3]} kicked from #{chan} by #{@users[$1].nick} (#{$4})")
+        
         if chan.users.size > 0
           @channels[$2] = chan
         else
           @channels.delete($2)
         end
+        
       elsif line =~ /^:(\w{9}) PART (#\w+)/
-        chan = @channels[$2]
-        chan.part($1)
-        nick = @users[$1].nick
-        $log.info("#{nick} parted #{chan}")
+        chan = @channels[$2].part($1)
+        $log.info("#{@users[$1]} parted #{chan}.")
+        
         if chan.users.size > 0
           @channels[$2] = chan
         else
           @channels.delete($2)
         end
+        
       else
         $log.info "Unhandled user input: #{line}"
       end
+      
     end
   
     def server_input(line)
@@ -219,29 +228,29 @@ module RServ::Protocols
       name = Configru.link.name
       
       if line =~ /^:(\w{3}) UID (\S+) (\d{1,2}) (\d{10}) \+([a-zA-Z]*) (\S+) (\S+) (\S+) ([0-9]\w{2}[A-Z][A-Z0-9]{5}) :(.*)$/
-        user = RServ::IRC::User.new($2, $9, $3, $5, $6, $7, $8, $10)
-        @users[user.uid] = user
-        $event.send("user::connected", user)
-        $log.info "New user #{user.uid} on #{user.sid} (#{@servers[$1].hostname}). Host: #{user.nick}!#{user.username}@#{user.hostname} (#{user.ip}) | Modes: +#{user.mode}"
+        @users[$2] = RServ::IRC::User.new($2, $9, $3, $5, $6, $7, $8, $10)
+        $event.send("user::connected", @users[$2])
+        $log.info "New user #{user.uid} on #{user.sid} (#{@servers[$1].hostname}). Host: #{user.nick}!#{user.username}@#{user.hostname} (#{user.ip}) | Modes: +#{user.mode}."
+  
       elsif line =~ /^:(\w{3}) SID (\S+) (\d{1,2}) ([0-9][0-9A-Z]{2}) :(.*)$/
         server = RServ::IRC::Server.new($4, $2, $3, $5)
-        $log.info "New server: #{server.hostname} (#{server.sid}) [#{server.gecos}]"
+        $log.info "New server: #{server.hostname} (#{server.sid}) [#{server.gecos}]."
         @servers[server.sid] = server
         $event.send("server::sid", server)
         send(":#{sid} PING #{name} :#{server.sid}")
+     
       elsif line =~ /^:([0-9]{1}[A-Z0-0]{2}) SJOIN (\d+) (\S+) (\+.*) :(.*)$/
-        chan = RServ::IRC::Channel.new($3, $2.to_i, $4, $5)
-        if @channels.has_key?(chan.name)
-          #panic!
-          $log.fatal "Channels out of sync, dying"
-          exit
-        end
-        @channels[chan.name] = chan
+        #the channel object logs its own creation
+        @channels[$3] = RServ::IRC::Channel.new($3, $2.to_i, $4, $5)
+     
       elsif line =~ /^:(\w{3}) PING (\S+) :(.*)$/
+        #this is only called when a remote server pings (i.e. not from the server we connect to)
         send(":#{sid} PONG #{name} :#{$1}")        
+      
       else
         $log.info "Unhandled server input: #{line}"
       end
+      
     end
     
 	end
