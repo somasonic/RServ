1ef893f3ab3d8ad0618ca596a07fd47180fb4a15
Andrew Northall <andy@somasonic.org>
2013-06-12 07:00:20 +0000

    Update control plugin to be more dynamic

--- plugins/control.rb
+++ plugins/control.rb
@@ -1,8 +1,7 @@
 class Control < RServ::Plugin
   
   def initialize
-    @control = RServ::IRC::PsuedoClient.new("RubyServices", "rserv", "rserv.interlinked.me", "RServ Services", "SZ", ["#services"])
-    @channel = "#services"
+    @control = RServ::IRC::PsuedoClient.new("RServ", "rserv", "rserv.interlinked.me", "RServ Services", "SZ", ["#rserv", "#services"])
         
     $event.add(self, :on_input, "link::input")
   end
@@ -13,46 +12,47 @@ class Control < RServ::Plugin
   
   def on_input(line)
     if line =~ /:(\w{9}) PRIVMSG (#\w+) :(\w+)\S{0,1} (.*)$/i
-      return unless $2 == @channel
+      return unless @control.channels.include?($2)
       return unless $3.downcase == @control.nick.downcase
+      c = $2
       user = $protocol.get_uid($1)
       if user.oper?
-        command(user, $4)
+        command(c, user, $4)
       else
-        chan("Sorry, you are not an IRC operator.")
+        msg(c, "Sorry, you are not an IRC operator.")
       end
     end
   end    
   
-  def command(user, command)
+  def command(c, user, command)
     if command =~ /^eval (.*)$/i
       begin
         result = eval($1)
-        chan("=> #{result.to_s}")
+        msg(c, "=> #{result.to_s}")
       rescue => e
-        chan("!| #{e}")
-        chan("=> #{$1}")
-        chan("!| #{e}")
-        chan("!| #{e.backtrace.join("\n")}")
+        msg(c, "!| #{e}")
+        msg(c, "=> #{$1}")
+        msg(c, "!| #{e}")
+        msg(c, "!| #{e.backtrace.join("\n")}")
       end
     elsif command =~ /^load (\w+)$/i
       begin
         RServ::Plugin.load($1)
-        chan("Plugin #{$1} loaded successfully.")
+        msg(c, "Plugin #{$1} loaded successfully.")
       rescue LoadError => e
-        chan("Error loading plugin #{$1}: #{e}")
+        msg(c, "Error loading plugin #{$1}: #{e}")
       end
     elsif command =~ /^unload (\w+)$/i
       begin
         RServ::Plugin.unload($1)
-        chan("Plugin #{$1} unloaded successfully.")
+        msg(c, "Plugin #{$1} unloaded successfully.")
       rescue => e
-        chan("Error unloading plugin #{$1}: #{e}")
+        msg(c, "Error unloading plugin #{$1}: #{e}")
       end
     end
   end
   
-  def chan(msg)
-    @control.privmsg(@channel, msg)
+  def msg(t, msg)
+    @control.privmsg(t, msg)
   end
 end
