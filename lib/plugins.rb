cfada3c5c24565ad8ca0d3b49beb55d83208df55
Andrew Northall <andy@somasonic.org>
2013-06-12 02:20:48 +0100

    Redo the plugin system somewhat so that it now works. Fixes #1.

--- lib/plugins.rb
+++ lib/plugins.rb
@@ -16,48 +16,46 @@ require 'lib/irc/psuedoclient'
 
 module RServ
   class Plugin
-		
-    @name = File.basename(__FILE__.split("/")[-1], '.rb')
-    
-    attr_accessor :clients, :name
+		    
+    attr_accessor :name
     
     def initialize
-      @clients = Array.new
+      @name = File.basename(__FILE__.split("/")[-1], '.rb')
     end
     
     def self.inherited(child)
       @children ||= []
       @instances ||= {}
       @children << child
-      @instances[child] = child.new
     end
 
     def self.load(f)
       begin
+        f = "plugins/#{f.downcase}.rb" unless f =~ /\.rb$/
         $log.info "Attempting to load plugin #{f}."
         Kernel.load(f)
         fn = File.basename(f, '.rb')
         klass = @children.find { |e| e.name.downcase == fn }
-        @instances[klass] = klass.new if klass
+        @instances[klass.to_s] = klass.new
+        @instances[klass.to_s].on_connect if $link.established
         $log.info "Loaded plugin #{f}."
       rescue => e
         $log.error "Error loading plugin #{f}. Error: #{e}\n#{e.backtrace.join("\n")}"
       end
     end
-
-
-    def self.unload(c)
-      klass = nil
-      if c.kind_of? Class 
-        klass = c
-      else 
-        klass = @children.find { |e| e.name.downcase == c }
+    
+    def self.unload(c)      
+      if @instances.has_key?(c)
+        klass = @instances[c]
+      else
+        return
+        # todo exception
       end
       klass.on_unload if klass.respond_to?("on_unload")
       $event.unregister(klass)
-      Object.send :remove_const, klass.name.intern
-      @children.delete klass
-      @instances.delete klass
+      @children.delete_if {|name| name.to_s == klass.class.to_s}
+      @instances.delete klass.class.to_s
+      Object.send :remove_const, klass.class.to_s
     end
 
     def self.list
