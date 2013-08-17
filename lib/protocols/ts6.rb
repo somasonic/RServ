3ebf81d82b3ca6efcd323e73d58db4066bcfd78f
Andrew Northall <andy@somasonic.org>
2013-08-17 09:31:21 +0100

    Merge Elizacats TS tracking pull request

--- lib/protocols/ts6.rb
+++ lib/protocols/ts6.rb
@@ -153,7 +153,7 @@ module RServ::Protocols
           end
           
         elsif line =~ /^:(\w{3}) UID (\S+) (\d{1,2}) (\d+) \+([a-zA-Z]*) (\S+) (\S+) (\S+) ([0-9]\w{2}[A-Z][A-Z0-9]{5}) :(.*)$/
-          user = RServ::IRC::User.new($2, $9, $3, $5, $6, $7, $8, $10, $4)
+          user = RServ::IRC::User.new($2, $9, $3, $5, $6, $7, $8, $10, $4.to_i)
           @users[user.uid] = user
           $log.info "New user #{user.uid} on #{user.sid}. Host: #{user.nick}!#{user.username}@#{user.hostname} (#{user.ip}) | Modes: +#{user.mode}"
           
@@ -161,10 +161,6 @@ module RServ::Protocols
           server = RServ::IRC::Server.new($4, $2, $3, $5)
           $log.info "New server: #{server.hostname} (#{server.sid}) [#{server.gecos}]"
           @servers[server.sid] = server
-
-        elsif line =~ /^:([0-9]{1}[A-Z0-9]{2}) NICK (\S+) :(\d+)$/
-          @users[$2].ts = $3
-          $log.info "New timestamp for user #{@users[$2]} on #{$1}: #{$3}"
 
         elsif line =~ /^:([0-9]{1}[A-Z0-9]{2}) SJOIN (\d+) (#.*) (\+.*) :(.*)$/
           if @channels.has_key?($3)
@@ -217,6 +213,7 @@ module RServ::Protocols
         
         old_nick = @users[$1].to_s
         @users[$1].nick = $2
+        @users[$1].ts = $3.to_i
         RServ::IRC::Command.new("nick", [old_nick, $2], $1)
       
       elsif line =~ /^:(\w{9}) QUIT :(.*)$/
@@ -314,8 +311,8 @@ module RServ::Protocols
       sid = Configru.link.serverid
       name = Configru.link.name
       
-      if line =~ /^:(\w{3}) UID (\S+) (\d{1,2}) (\d{10}) \+([a-zA-Z]*) (\S+) (\S+) (\S+) ([0-9]\w{2}[A-Z][A-Z0-9]{5}) :(.*)$/
-        user = RServ::IRC::User.new($2, $9, $3, $5, $6, $7, $8, $10)
+      if line =~ /^:(\w{3}) UID (\S+) (\d{1,2}) (\d+) \+([a-zA-Z]*) (\S+) (\S+) (\S+) ([0-9]\w{2}[A-Z][A-Z0-9]{5}) :(.*)$/
+        user = RServ::IRC::User.new($2, $9, $3, $5, $6, $7, $8, $10, $4.to_i)
         @users[user.uid] = user
         $event.send("user::connected", @users[$9])
         $log.info "New user #{user.uid} on #{user.sid} (#{@servers[$1].hostname}). Host: #{user.nick}!#{user.username}@#{user.hostname} (#{user.ip}) | Modes: +#{user.mode}."
