eaa03bb33e44092ee4d47f6f3004ba863755f9cd
Andrew Northall <andy@somasonic.org>
2013-10-15 23:34:52 +0100

    Remove fluff

--- plugins/sporks.rb
+++ plugins/sporks.rb
@@ -29,24 +29,6 @@ class Sporks < RServ::Plugin
   def initialize
     @control = RServ::IRC::PsuedoClient.new("Sporks", "sporks", "rserv.interlinked.me", "Utensil Services", "S", ["#Sporks", "#opers", "#realtalk"])
     
-    @user = nil
-    @lastlog = Hash.new
-    
-    begin
-      @karma = load("data/karma")
-    rescue
-      @karma = Hash.new
-      save(@karma, "data/karma")
-    end
-    
-    begin 
-      @strings = load("data/strings")
-    rescue
-      @strings = Hash.new
-      @strings["help"] = "Use ;set [stringname] [content] to set a string. Use ;[stringname] to recall."
-      save(@strings, "data/strings")
-    end
-    
     $event.add(self, :on_input, "link::input")
   end
   
@@ -59,66 +41,18 @@ class Sporks < RServ::Plugin
       return unless @control.channels.include?($2)
       user = $protocol.get_uid($1)
       command($2, user, $3)
-      @lastlog[$2] ||= Hash.new
-      @lastlog[$2][$1] = $3
     end
   end
   
   private
   
-  def load(file)
-    f = File.open(file, 'r')
-    data = JSON.load(f)
-    f.close
-    data
-  end
-
-  def save(data, file)
-    f = File.open(file, 'w')
-    JSON.dump(data, f)
-    f.flush
-    f.close
-  end
-  
   def command(chan, user, command)
-    if command =~ /\[\[(\S+)\]\]/
-      page = $1
-      return if page =~ /sroracle/i
-      @control.privmsg(chan, "[[#{page}]]: https://wiki.interlinked.me/page/#{page}")
-    elsif command =~ /^!trigger\s*/i
+    if command =~ /^!trigger\s*/i
       @control.privmsg(chan, "#{RED}#{BOLD} --- TRIGGER WARNING --- DO NOT READ IF YOU ARE EASILY OFFENDED OR CANNOT TAKE A JOKE ---#{RED}#{BOLD}")
     elsif command =~ /^!rules\s*$/i
       @control.privmsg(chan, "Network rules: https://wiki.interlinked.me/page/Network_Rules || Channel rules: https://wiki.interlinked.me/page/Sporks/rules || Summary: Don't be a dick!")
     elsif command =~ /^!wiki\s*$/i
       @control.privmsg(chan, "https://wiki.interlinked.me/ - please register to edit :)")
-    elsif command =~ /^!(hosts?|r?dns) (\S+)\s*$/i
-      addr = $2
-      resolver = Resolv::DNS.new
-      addrs = resolver.getaddresses(addr)
-      if addrs.empty?
-        begin
-          addrs = resolver.getnames(addr)
-          if addrs.empty?
-            @control.privmsg(chan, "No hosts found for #{addr}.")
-          else
-            reply = "Hosts for #{addr}: "
-            addrs.each {|a| reply = reply + a.to_s + " " }
-            @control.privmsg(chan, reply)
-          end
-        rescue Resolv::ResolvError
-          @control.privmsg(chan, "No hosts found for #{addr}.")
-        end
-      else
-        reply = "Hosts for #{addr}: "
-        addrs.each {|a| reply = reply + a.to_s + " " }
-        @control.privmsg(chan, reply)
-      end
-    elsif command =~ /^!karma\s*$/i
-      top = ordered_karma()[0..9]
-      str = "[Top Karma] "
-      cnt = 1
-      top.each { |k,v| str = str + "[#{cnt}] #{k} => #{v} "; cnt += 1}
-      @control.notice(user.uid, str)
     elsif command =~ /^!(lastquote|quote last)\s*$/i
       begin
         quote = get_last_quote()
@@ -147,33 +81,6 @@ class Sporks < RServ::Plugin
       else
         msg(chan, "[#{quote['id']}] [#{Time.at(quote['date'].to_i).utc}] [Rating: #{quote['rating']}] [URL: #{Googl.shorten("https://quotes.interlinked.me/?#{quote['id']}").short_url} ] #{CGI.unescape_html(quote['quote'].gsub(/\r\n/, ' | '))}")
       end
-    elsif command =~ /^!karma (\S+)\s*$/
-      @control.privmsg(chan, "Karma for #{$1}: #{@karma[$1.downcase]}")
-    elsif command =~ /^;set (\S+) (.+)\s*$/i
-      @strings[$1] = $2.strip
-      save(@strings, "data/strings")
-      msg(chan, "#{$1} => #{$2}")
-    elsif command =~ /^;(\S+)\s*$/i
-      if @strings.has_key?($1)
-        value = @strings[$1]
-        if value[0] == "$"
-          msg(chan, value[1..-1])
-        else
-          msg(chan, "#{$1} => #{@strings[$1]}")
-        end
-      end
-    elsif command =~ /^s\/(.+)\/?$/i
-      one = $1.split("/")[0..-2].join("/")
-      two = $1.split("/")[-1]
-      msg(chan, "#{user.nick} meant \"#{@lastlog[chan][user.uid].gsub(one, two)}\".")
-    elsif command =~ /^(\S+)(:|\?) s\/(.+)\/?$/i
-      suser = $protocol.get_user($1)
-      return if user.nil?
-      one = $3.split("/")[0..-2].join("/")
-      two = $3.split("/")[-1]
-      msg(chan, "#{user.nick} thinks #{suser.nick} meant \"#{@lastlog[chan][suser.uid].gsub(one, two)}\".")
-    elsif command =~ /(\S)+\+\+/ or command =~ /(\S)+--/
-      karma_process(command, user)
     end
   end
 
@@ -204,28 +111,6 @@ class Sporks < RServ::Plugin
     JSON.parse(response)
   end
 
-  def karma_process(line, origin)
-    words = line.split(" ")
-    changed = 0 
-    words.each do
-      |w|
-      if w =~ /^(.+)\+\+$/
-        stripped = $1
-        @karma[stripped.downcase] += 1 unless stripped =~ /#{origin.nick}/i
-        changed = 1
-      elsif w =~ /^(.+)\-\-$/
-        stripped = $1
-        @karma[stripped.downcase] -= 1 unless stripped =~ /#{origin.nick}/i
-        changed = 1
-      end
-    end
-    save(@karma, "data/karma")
-  end
-  
-  def ordered_karma
-    @karma.sort_by{|k,v| v}.reverse
-  end
-  
   def msg(t, msg)
     @control.privmsg(t, msg)
   end
